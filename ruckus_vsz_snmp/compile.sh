94eb343be55d80a8d15c10cdd115a947ce76ae87
Lukas Schauer <lukas@schauer.so>
2016-05-19 17:41:27 +0200

    added additional snmp related files

---
+++ ruckus_vsz_snmp/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+rm -f RUCKUS*.py
+
+smidump -f python -k RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-ROOT-MIB.py
+smidump -f python -k RUCKUS-TC-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-TC-MIB.py
+smidump -f python -k RUCKUS-PRODUCTS-MIB.txt -p RUCKUS-TC-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-PRODUCTS-MIB.py
+
+smidump -f python -k RUCKUS-SCG-EVENT-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-SCG-EVENT-MIB.py
+smidump -f python -k RUCKUS-SCG-SYSTEM-MIB.txt -p RUCKUS-TC-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-SCG-SYSTEM-MIB.py
+
+smidump -f python -k RUCKUS-SCG-CONFIG-WLAN-MIB.txt -p RUCKUS-TC-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-SCG-CONFIG-WLAN-MIB.py
+smidump -f python -k RUCKUS-SCG-WLAN-MIB.txt -p RUCKUS-TC-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-SCG-WLAN-MIB.py
+smidump -f python -k RUCKUS-SCG-TTG-MIB.txt -p RUCKUS-TC-MIB.txt -p RUCKUS-ROOT-MIB.txt | libsmi2pysnmp > RUCKUS-SCG-TTG-MIB.py
+
+echo "All done!"
+
