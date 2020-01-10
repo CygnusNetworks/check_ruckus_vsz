e761dffafed4c748d2f2544c3ad98a088a3ab2c2
Dr. Torge Szczepanek <info@cygnusnetworks.de>
2020-01-10 13:35:53 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 from setuptools import setup
 
 setup(name='check_ruckus_vsz',
-	version='0.13',
+	version='0.14',
 	description='Nagios Check for Ruckus SCG',
-	author='Lukas Schauer',
-	author_email='l.schauer@cygnusnetworks.de',
+	author='Lukas Schauer, Dr. Torge Szczepanek',
+	author_email='info@cygnusnetworks.de',
 	license='Apache 2.0',
 	packages=['ruckus_vsz_snmp'],
 	scripts=['check_ruckus_vsz', 'check_ruckus_ap'],
